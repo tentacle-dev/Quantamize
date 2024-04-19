bf4080c4a08d473540c1d1cbfe2cdd9044bd4423
Thanush <tanush0525@gmail.com>
2024-04-19 11:14:08 -0400

    displayProduct function

--- scripts/botScript.js
+++ scripts/botScript.js
@@ -11,7 +11,62 @@ if (!indexedDB) {
   console.log("IndexedDB could not be found in this browser.");
 }
 
-function displayProducts() {}
+function displayProducts() {
+  const dbName = "Quantamize";
+  const request = window.indexedDB.open(dbName);
+
+  request.onerror = function () {
+    console.error("Failed to open database");
+  };
+
+  request.onsuccess = function (event) {
+    const db = event.target.result;
+    const transaction = db.transaction(["product"], "readwrite");
+    const productStore = transaction.objectStore("product");
+
+    const productListDiv = document.getElementById("product-list");
+
+    productStore.openCursor().onsuccess = function (event) {
+      const cursor = event.target.result;
+      if (cursor) {
+        const product = cursor.value;
+        console.log(product);
+        if (product.type == "Robot") {
+          const productDiv = document.createElement("div");
+          productDiv.classList.add("product"); // Add a class to the product div
+          productDiv.innerHTML = `
+                        
+
+                <div class="w-full max-w-sm bg-white border border-gray-200 rounded-lg shadow dark:bg-gray-800 dark:border-gray-700 cursor-pointer ">
+                    
+                        <img class="p-1 rounded-t-lg product-img" src="${product.image_path}" />
+                    <div class="px-5 pb-5 mt-2">
+                        <div>
+                            <h5 class="text-xl font-semibold tracking-tight text-gray-900 dark:text-white">${product.name}</h5>
+                            <h5 class="text-md tracking-tight text-gray-900 dark:text-gray-100">${product.description}</h5>
+                        </div>
+                        
+                        <div class="flex items-center justify-between lg:mt-1">
+                            <span class="text-3xl font-bold text-gray-900 dark:text-white">${product.price}</span>
+                            <a class="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">Add to cart</a>
+                        </div>
+                    </div>
+                </div>
+
+                    `;
+          // Add click event listener to the product div
+          productDiv.addEventListener("click", function () {
+            addToCart(product); // Call function to add product to cart
+          });
+          productListDiv.appendChild(productDiv);
+        }
+        cursor.continue();
+      } else {
+        console.log("All products displayed");
+      }
+    };
+  };
+}
 
 window.onload = () => {
   displayProducts();
