e410882212722f0f72e1d18f94c0e76995235b9f
Sneh a Jeshwin <snehajeshwin1995@gmail.com>
2024-04-19 17:02:32 -0400

    Updated code for registration page, about us page and scripts

--- scripts/registration.js
+++ scripts/registration.js
@@ -172,3 +172,16 @@ function handleRegistrationForm(event) {
 // Add event listener to registration form for form submission
 const registrationForm = document.getElementById("registrationForm");
 registrationForm.addEventListener("submit", handleRegistrationForm);
+
+document.addEventListener("DOMContentLoaded", function () {
+    const signinLink = document.getElementById("signin-link");
+    const userId = localStorage.getItem("user_id");
+  
+    if (userId) {
+      signinLink.href = "cart.html";
+      signinLink.innerText = "My Cart";
+    } else {
+      signinLink.href = "login.html";
+      signinLink.innerText = "Sign in";
+    }
+  });
