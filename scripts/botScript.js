84aa0bb5015ed94eca81ae6b6223183422bbc5b9
Thanush <tanush0525@gmail.com>
2024-04-19 11:12:08 -0400

    adds botscript

---
+++ scripts/botScript.js
@@ -0,0 +1,18 @@
+// chooses the first one that is available/found
+const indexedDB =
+  window.indexedDB ||
+  window.mozIndexedDB ||
+  window.webkitIndexedDB ||
+  window.msIndexedDB ||
+  window.shimIndexedDB;
+
+// Some browsers may not support
+if (!indexedDB) {
+  console.log("IndexedDB could not be found in this browser.");
+}
+
+function displayProducts() {}
+
+window.onload = () => {
+  displayProducts();
+};
